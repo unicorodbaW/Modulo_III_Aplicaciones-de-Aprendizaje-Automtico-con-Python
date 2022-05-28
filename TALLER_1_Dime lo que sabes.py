a5f751f112ebf5d1657c06433016facbbbc2ba1d
Wendy Mendoza <105951977+unicorodbaW@users.noreply.github.com>
2022-05-28 15:16:01 -0500

    Solucion Dime lo que sabes

---
+++ TALLER_1_Dime lo que sabes.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 28 14:44:25 2022
+
+@author: Wendy Mendozita
+"""
+
+a=int(input("ingrese el valor de a: "))
+b=int(input("ingrese el valor de b: "))
+c=int(input("ingrese el valor de c: "))
+d=int(input("ingrese el valor de d: "))
+e=int(input("ingrese el valor de e: "))
+print("")
+
+ecu1=(a+(b/c))/(d+(e/f))
+ecu2=(a-(b/(c-d)))
+print("")
+print("Ecuacion 1")
+print(ecu1)
+print("")
+print("Ecuacion 2")
+print(ecu2)
+
+print("")
+aux= ecu1
+ecu1= ecu2
+ecu2=aux
+#print(ecu1)
+#print(ecu2)
+
+print(f"valor de ecu1 es: {ecu1}")
+print(f"valor de ecu2 es: {ecu2}")

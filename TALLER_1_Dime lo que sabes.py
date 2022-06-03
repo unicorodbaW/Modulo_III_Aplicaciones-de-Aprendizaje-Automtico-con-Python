faa72f72be6046854785a1f6b2ef25ec5485c3fa
Wendy Mendoza <105951977+unicorodbaW@users.noreply.github.com>
2022-06-03 16:49:36 -0500

    Solucion del Taller_1

--- TALLER_1_Dime lo que sabes.py
+++ TALLER_1_Dime lo que sabes.py
@@ -5,13 +5,15 @@ Created on Sat May 28 14:44:25 2022
 @author: Wendy Mendozita
 """
 
-a=int(input("Ingrese el valor de a: "))
-b=int(input("Ingrese el valor de b: "))
-c=int(input("Ingrese el valor de c: "))
-d=int(input("Ingrese el valor de d: "))
-e=int(input("Ingrese el valor de e: "))
-f=int(input("Ingrese el valor de f: "))
+## Nombre:  Wendy Paola Mendoza Barrera
+## ID: 502216
+## Email: wendy.mendozab@upb.edu.co 
 
+a=int(input("ingrese el valor de a: "))
+b=int(input("ingrese el valor de b: "))
+c=int(input("ingrese el valor de c: "))
+d=int(input("ingrese el valor de d: "))
+e=int(input("ingrese el valor de e: "))
 print("")
 
 ecu1=(a+(b/c))/(d+(e/f))
